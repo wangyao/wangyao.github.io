601300cde97108b086fbd074c5d56d631cba26e5
王瑶 <wangyaobeijing@163.com>
2022-03-13 10:09:32 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
    https://github.com/P233/3-Jekyll
 \*---------------------------------*/
 
-cnzz
+// cnzz
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1280965679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1280965679%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
 
